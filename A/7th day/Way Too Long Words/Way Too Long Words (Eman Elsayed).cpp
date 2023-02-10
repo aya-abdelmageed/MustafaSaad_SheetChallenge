e64f7c3147747d6b71eee13421b0f29561abac93
EmanMohamed <EmanMohamed@DESKTOP-8KCBCII>
2023-02-10 18:47:29 +0200

    8th day problem

--- A/7th day/Way Too Long Words/Way Too Long Words (Eman Elsayed).cpp
+++ A/7th day/Way Too Long Words/Way Too Long Words (Eman Elsayed).cpp
@@ -1,11 +1,14 @@
 // author : Eman Elsayed
+#include <bits/stdc++.h>
+using namespace std;
 
 int main()
 {
     string str;
     cin >> str;
-    if (sz(str) > 10) // if string length is strictly greater than 10
-        cout << str[0] << sz(str) - 2 << str[sz(str) - 1] << endl;
+    int sz = str.size();
+    if (sz > 10) // if string length is strictly greater than 10
+        cout << str[0] << sz - 2 << str[sz - 1] << endl;
     else // else print the string as it is
         cout << str << endl;
 
---
+++ A/8th day/README.md
@@ -0,0 +1,32 @@
+# 7th Day of Mustafa Saad sheet Challenge
+
+<br><br>
+
+## Problems:
+
+1. [Helpful Maths](https://codeforces.com/contest/339/problem/A)
+2. [Team Olympiad](https://codeforces.com/contest/490/problem/A)
+
+<hr>
+
+<br><br>
+
+## 1) [Helpful Maths](https://codeforces.com/contest/339/problem/A)
+
+### Code
+
+```cpp
+Wating
+```
+
+<hr>
+
+<br><br>
+
+## 2) [Team Olympiad](https://codeforces.com/contest/490/problem/A)
+
+### Code
+
+```cpp
+Wating
+```
